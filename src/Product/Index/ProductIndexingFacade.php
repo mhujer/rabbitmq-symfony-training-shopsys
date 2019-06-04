1924c052373e4482029ace8f7f3e79839069ea17
Martin Hujer <mhujer@gmail.com>
2019-06-04 15:21:48 +0200

    forceStopConsumer() if EM is closed

--- src/Product/Index/ProductIndexingFacade.php
+++ src/Product/Index/ProductIndexingFacade.php
@@ -23,8 +23,17 @@ final class ProductIndexingFacade
     {
         $product = $this->entityManager->getRepository(Product::class)->find($productId);
 
+        // not a good example, I just need a DB update/insert here
+        $productCopy = new Product($product->getName() . '-2', $product->getPrice());
+        $this->entityManager->persist($productCopy);
+        $this->entityManager->flush();
+
+
         if (random_int(1, 100) < 50) {
-            throw new \Exception('Service is not available, processing failed');
+            //throw new \Exception('Service is not available, processing failed');
+            $productClone = clone $product;
+            $this->entityManager->persist($productClone);
+            $this->entityManager->flush();
         }
 
         // do some hard indexing here
--- src/Product/Index/ReindexProductConsumer.php
+++ src/Product/Index/ReindexProductConsumer.php
@@ -4,25 +4,31 @@ namespace App\Product\Index;
 
 use Doctrine\ORM\EntityManagerInterface;
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
+use OldSound\RabbitMqBundle\RabbitMq\DequeuerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
 use Ramsey\Uuid\Uuid;
 
-final class ReindexProductConsumer
+final class ReindexProductConsumer // ConsumerCallback
     implements \OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface
 {
 
     /** @var \App\Product\Index\ProductIndexingFacade */
     private $productIndexingFacade;
 
+    /** @var \OldSound\RabbitMqBundle\RabbitMq\DequeuerInterface */
+    private $dequeuer;
+
     /** @var \Doctrine\ORM\EntityManagerInterface */
     private $entityManager;
 
     public function __construct(
         ProductIndexingFacade $productIndexingFacade,
+        DequeuerInterface $dequeuer,
         EntityManagerInterface $entityManager
     )
     {
         $this->productIndexingFacade = $productIndexingFacade;
+        $this->dequeuer = $dequeuer;
         $this->entityManager = $entityManager;
     }
 
@@ -39,11 +45,18 @@ final class ReindexProductConsumer
         try {
             $product = $this->productIndexingFacade->reindexProduct($productId);
         } catch (\Exception $e) { // @todo should be specific exception!
+            echo $e->getMessage() . "\n";
             echo 'Indexing failed, requeueing' . "\n";
 
             sleep(2);
 
             return ConsumerInterface::MSG_REJECT_REQUEUE;
+        } finally {
+            if (!$this->entityManager->isOpen()) {
+                echo 'EM is closed, shutting down' . "\n";
+                $this->dequeuer->forceStopConsumer();
+                sleep(2);
+            }
         }
         echo 'Indexed product ' . $product->getName() . "\n";
 
